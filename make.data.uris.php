23915c2c04166992a3828dc85b7166fce21ca933
Sam Trenholme <strenholme.usenet@gmail.com>
2018-06-03 13:39:26 -0700

    Added file with WOFF2 form of data URI

--- make.data.uris.php
+++ make.data.uris.php
@@ -1,14 +1,38 @@
+#!/usr/bin/php
 <?php
-# We use WOFF, not WOFF 2 for maximum cross browser compatibility
-if($argc != 3) {
-	print "Usage: " . $argv[0] . " {WOFF font file} {font name}\n";
+# Parse args
+if($argc != 3 && $argc != 4) {
+	print "Usage: " . $argv[0] . " {WOFF(2) font file} {font name}\n";
 	exit(0);
 }
+
+# Load and base64 convert font file
 $a = file_get_contents($argv[1]);
 $b = base64_encode($a);
-print "@font-face {";
+
+# See if this is a bold or italix font
+$more = "";
+if($argc == 4) {
+    if(preg_match('/B/',$argv[3])) {
+	$more = "font-weight: bold";
+    } else {
+        $more = "font-style: italic";
+    }
+}
+
+# Determine MIME type
+$type = "application/x-font-woff";
+if(preg_match('/woff2$/',$argv[1])) {
+    $type = "application/font-woff2";
+}
+
+# Output CSS
+print "@font-face {\n";
 print "\tfont-family: " . $argv[2] . ";\n";
-print "\tsrc:url(data:application/x-font-woff;base64," . $b . 
+print "\tsrc:url(data:" . $type . ";base64," . $b . 
 	") format('woff');\n";
-print "}";
+if($more) {
+	print "\t" . $more . ";\n";
+}
+print "}\n";
 ?>
